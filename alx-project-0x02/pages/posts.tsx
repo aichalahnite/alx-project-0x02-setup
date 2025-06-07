aeffd90e96d5d0d8eafe6b16c5541035c62830a6
aichalahnite <159141036+aichalahnite@users.noreply.github.com>
2025-06-07 15:14:48 +0100

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,13 +1,22 @@
 import { useEffect, useState } from 'react';
-import PostCard from '../components/common/PostCard';
+import PostCard from '@/components/common/PostCard';
+import Header from '@/components/layout/Header';
 
-const [posts, setPosts] = useState([]);
-useEffect(() => {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(setPosts);
-}, []);
-
-return posts.map(post => (
-  <PostCard key={post.id} {...post} />
-));
+export default function Posts() {
+  return (
+    <>
+      <Header />
+      <h1>Posts Page</h1>
+      const [posts, setPosts] = useState([]);
+      useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+          .then(res => res.json())
+          .then(setPosts);
+      }, []);
+      
+      return posts.map(post => (
+        <PostCard key={post.id} {...post} />
+      ));
+    </>
+  );
+}
