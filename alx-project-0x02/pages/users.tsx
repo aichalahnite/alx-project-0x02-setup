9ceb1c843ab5a1c27e69ddbf0fc34c6455e96571
aichalahnite <159141036+aichalahnite@users.noreply.github.com>
2025-06-07 15:26:04 +0100

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,13 +1,32 @@
-import { useEffect, useState } from 'react';
+import Header from '@/components/layout/Header';
 import UserCard from '@/components/common/UserCard';
+import { type UserProps } from '@/interfaces';
 
-const [users, setUsers] = useState([]);
-useEffect(() => {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(setUsers);
-}, []);
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-return users.map(user => (
-  <UserCard key={user.id} {...user} />
-));
+export default function Users({ users }: UsersPageProps) {
+  return (
+    <>
+      <Header />
+      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      <div className="space-y-4">
+        {users.map(user => (
+          <UserCard key={user.id} {...user} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: UserProps[] = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
